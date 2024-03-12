8f88c63f289f873b4c3fe8240c111858ef152bc1
Gg <drgkng9@gmail.com>
2024-03-12 10:57:09 +0545

    prunable by updated at

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -13,11 +13,17 @@ class Order extends Model
     use Prunable;
 
     protected $fillable = ['number', 'order_key', 'status', 'date_created', 'total', 'customer_id', 'customer_note',
-        'billing', 'shipping',];
+        'billing', 'shipping','created_at','updated_at'];
     protected $casts = [
         'billing'=>'json', 'shipping'=>'json'
     ];
-
+    /**
+     * Get the prunable model query.
+     */
+    public function prunable(): Builder
+    {
+        return static::where('updated_at', '<=', now()->subDays(2));
+    }
     public function lineItems():HasMany
     {
         return $this->hasMany(LineItem::class);
